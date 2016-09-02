f1f1c30eded8c71e00376829d7abe0c7b31ef19a
Florian Ehmke <imba3r@gmail.com>
2016-09-02 13:06:39 +0200

    Added firewall.

--- openvpn/start.sh
+++ openvpn/start.sh
@@ -45,4 +45,18 @@ if [ -n "${LOCAL_NETWORK-}" ]; then
   fi
 fi
 
+# firewall all non-vpn traffic
+remote_port=$(grep 'remote ' "${OPENVPN_CONFIG}" | awk '{print $3}')
+docker_network=$(ip -o addr show dev eth0 | awk '$3 == "inet" {print $4}')
+iptables -F OUTPUT
+iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
+iptables -A OUTPUT -o tap0 -j ACCEPT
+iptables -A OUTPUT -o tun0 -j ACCEPT
+iptables -A OUTPUT -d ${docker_network} -j ACCEPT
+iptables -A OUTPUT -p udp -m udp --dport 53 -j ACCEPT
+iptables -A OUTPUT -p tcp -m tcp --dport ${remote_port} -j ACCEPT
+iptables -A OUTPUT -p udp -m udp --dport ${remote_port} -j ACCEPT;
+iptables -A OUTPUT -j DROP
+
 exec openvpn $OPENVPN_OPTS --config "$OPENVPN_CONFIG"
